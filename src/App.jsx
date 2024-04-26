3f04e9ef812d31bd775d14030c72d1fb02a73748
Mauro Valdez <josenova0@gmail.com>
2024-04-26 17:04:39 -0300

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/titan-one'
 
 import './App.css'
 
 function App() {
 
-  
 }
 
 export default App
