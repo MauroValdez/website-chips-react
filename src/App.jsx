ea3c62314cb0aa55b5d6b56daacb0c1d44b53f19
Mauro Valdez <josenova0@gmail.com>
2024-04-29 18:01:13 -0300

    Care

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Header from './layouts/Header'
 import Home from './layouts/Home'
 import Favorites from './layouts/Favorites'
+import Care from './layouts/Care'
 function App() {
     return (
         <>
@@ -13,6 +14,7 @@ function App() {
             <main className="main">
                 <Home />
                 <Favorites />
+                <Care />
             </main>
             <section className="section"></section>
             <section className="section"></section>
