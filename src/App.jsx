dab977dc7cc5731bc9dcaff0d8283ebbdb4897f4
Mauro Valdez <josenova0@gmail.com>
2024-04-29 22:51:32 -0300

    Scroll-Bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import '@fontsource/poppins/400.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/titan-one'
 import './App.css'
+import './layouts/ScrollBar.css'
 
 import Header from './layouts/Header'
 import Home from './layouts/Home'
